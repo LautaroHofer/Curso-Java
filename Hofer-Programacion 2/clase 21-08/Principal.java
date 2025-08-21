cd7ead45d6481c762128141a0980f7b51a0a8b2d
Lautaro Hofer <lautarohofer@gmail.com>
2025-08-21 20:50:08 -0300

    Delete Hofer-Programacion 2/clase 21-08 directory

--- Hofer-Programacion 2/clase 21-08/Principal.java
+++
@@ -1,7 +0,0 @@
-public class Principal {  
-    public static void main (String[] args) {
-        Mascota firulais = new Mascota();
-        firulais.nombre = "julio";
-        firulais.identificarse ();
-    }
-}
